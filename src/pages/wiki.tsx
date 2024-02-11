2317074ec01a94b7c8741854f4d943bfb74f28d6
do058036 <do058036@intelbras.com.br>
2024-02-11 13:01:11 -0300

    remove wiki

--- src/pages/wiki.tsx
+++ src/pages/wiki.tsx
@@ -22,44 +22,44 @@ export default function Wiki({ items, npcs, crafts }: Props): JSX.Element {
     index: 0
   });
 
-  npcs.entries.map(item => {
-    item.Value.Drops.map(x => {
-      let name = items.entries.find(y => y.Key === x.ItemId);
-      if (name) {
-        x.ItemId = name.Value.Name;
-        x.Icon = name.Value.Icon;
-        return;
-      }
-      return;
-    });
-  });
+  // npcs.entries.map(item => {
+  //   item.Value.Drops.map(x => {
+  //     let name = items.entries.find(y => y.Key === x.ItemId);
+  //     if (name) {
+  //       x.ItemId = name.Value.Name;
+  //       x.Icon = name.Value.Icon;
+  //       return;
+  //     }
+  //     return;
+  //   });
+  // });
 
-  crafts.entries.map(item => {
-    let flag = items.entries.find(y => y.Key === item.Value.ItemId);
-    if (flag) item.Value.Icon = flag.Value.Icon;
+  // crafts.entries.map(item => {
+  //   let flag = items.entries.find(y => y.Key === item.Value.ItemId);
+  //   if (flag) item.Value.Icon = flag.Value.Icon;
 
-    item.Value.Ingredients.map(x => {
-      let name = items.entries.find(y => y.Key === x.ItemId);
-      if (name) {
-        x.ItemId = name.Value.Name;
-        x.Icon = name.Value.Icon;
-        return;
-      }
-      return;
-    });
-  });
+  //   item.Value.Ingredients.map(x => {
+  //     let name = items.entries.find(y => y.Key === x.ItemId);
+  //     if (name) {
+  //       x.ItemId = name.Value.Name;
+  //       x.Icon = name.Value.Icon;
+  //       return;
+  //     }
+  //     return;
+  //   });
+  // });
 
   return (
     <div>
       <main>
         <Layout>
-          <WikiPage
+          {/* <WikiPage
             activityWiki={activityWiki}
             callback={(e: ActivityWiki) => setActivityWiki(e)}
             items={items}
             npcs={npcs}
             crafts={crafts}
-          />
+          /> */}
         </Layout>
       </main>
     </div>
@@ -67,25 +67,25 @@ export default function Wiki({ items, npcs, crafts }: Props): JSX.Element {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const api = getAPIPublic();
+  // const api = getAPIPublic();
 
-  const { data: items } = await api.post('/v1/gameobjects/item', {
-    page: 0,
-    count: 100
-  });
+  // const { data: items } = await api.post('/v1/gameobjects/item', {
+  //   page: 0,
+  //   count: 100
+  // });
 
-  const { data: npcs } = await api.post('/v1/gameobjects/npc', {
-    page: 0,
-    count: 100
-  });
+  // const { data: npcs } = await api.post('/v1/gameobjects/npc', {
+  //   page: 0,
+  //   count: 100
+  // });
 
-  const { data: crafts } = await api.post('/v1/gameobjects/crafts', {
-    page: 0,
-    count: 100
-  });
+  // const { data: crafts } = await api.post('/v1/gameobjects/crafts', {
+  //   page: 0,
+  //   count: 100
+  // });
 
   return {
-    props: { items, npcs, crafts },
+    props: {},
     revalidate: 3600
   };
 };
